0ba1f3b095b8b70466ae6c668fd34a940bc8926e
Csahoo009 <sahoochiranjeevi009@gmail.com>
2026-03-23 16:30:43 +0530

    changes

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import java.util.*;
 
 class Service {
@@ -49,5 +50,60 @@ public class BookMyStayApp {
 
         System.out.println("Reservation ID: " + resId);
         System.out.println("Total Add-On Cost: " + manager.calculateTotalServiceCost(resId));
+=======
+import java.util.ArrayList;
+import java.util.List;
+
+class Reservation {
+    private String guestName;
+    private String roomType;
+
+    public Reservation(String guestName, String roomType) {
+        this.guestName = guestName;
+        this.roomType = roomType;
+    }
+
+    public String getGuestName() { return guestName; }
+    public String getRoomType() { return roomType; }
+}
+
+class BookingHistory {
+    private List<Reservation> confirmedReservations;
+
+    public BookingHistory() {
+        confirmedReservations = new ArrayList<>();
+    }
+
+    public void addReservation(Reservation reservation) {
+        confirmedReservations.add(reservation);
+    }
+
+    public List<Reservation> getConfirmedReservations() {
+        return confirmedReservations;
+    }
+}
+
+class BookingReportService {
+    public void generateReport(BookingHistory history) {
+        System.out.println("Booking History Report");
+        for (Reservation res : history.getConfirmedReservations()) {
+            System.out.println("Guest: " + res.getGuestName() + ", Room Type: " + res.getRoomType());
+        }
+    }
+}
+
+public class BookMyStayApp {
+    public static void main(String[] args) {
+        System.out.println("Booking History and Reporting\n");
+
+        BookingHistory history = new BookingHistory();
+        BookingReportService reportService = new BookingReportService();
+
+        history.addReservation(new Reservation("Abhi", "Single"));
+        history.addReservation(new Reservation("Subha", "Double"));
+        history.addReservation(new Reservation("Vanmathi", "Suite"));
+
+        reportService.generateReport(history);
+>>>>>>> feature/UC8
     }
 }
